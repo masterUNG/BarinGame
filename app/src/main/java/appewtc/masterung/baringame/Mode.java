585cee9afbb362768aef90f6b8ee0d32901f57f3
masterUNG <phrombutr@gmail.com>
2017-01-15 00:54:41 +0700

    Background Change OK

--- app/src/main/java/appewtc/masterung/baringame/Mode.java
+++ app/src/main/java/appewtc/masterung/baringame/Mode.java
@@ -1,13 +1,52 @@
 package appewtc.masterung.baringame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class Mode extends AppCompatActivity {
+public class Mode extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private ImageView[] imageViews = new ImageView[4];
+    private int[] ints = new int[]{R.id.imageButton, R.id.imageButton2,
+            R.id.imageButton3, R.id.imageButton4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
-    }
-}
+
+        //Bind Widget and Image Controller
+        for (int i = 0; i < imageViews.length; i++) {
+            imageViews[i] = (ImageView) findViewById(ints[i]);
+            imageViews[i].setOnClickListener(Mode.this);
+        }   // for
+
+    }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
+        int i = 0;
+        switch (view.getId()) {
+            case R.id.imageButton:
+                i = 0;
+                break;
+            case R.id.imageButton2:
+                i = 1;
+                break;
+            case R.id.imageButton3:
+                i = 2;
+                break;
+            case R.id.imageButton4:
+                i = 3;
+                break;
+        }   // switch
+        Intent intent = new Intent(Mode.this, PlayGame.class);
+        intent.putExtra("Index", i);
+        startActivity(intent);
+    }   // onClick
+
+}   // Main Class
---
+++ app/src/main/java/appewtc/masterung/baringame/PlayGame.java
@@ -0,0 +1,35 @@
+package appewtc.masterung.baringame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+public class PlayGame extends AppCompatActivity {
+
+    //Explicit
+    private RelativeLayout relativeLayout;
+    private int indexAnInt;
+    private String tag = "15janV1";
+    private int[] backgroundInts = new int[]{R.drawable.bg0, R.drawable.bg1,
+            R.drawable.bg2, R.drawable.bg3};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+
+        //Bind Widget
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_play_game);
+
+        //Setup
+        indexAnInt = getIntent().getIntExtra("Index", 0);
+        Log.d(tag, "ค่าที่ส่งมาจาก Mode ==> " + indexAnInt);
+
+        //Change View
+        relativeLayout.setBackgroundResource(backgroundInts[indexAnInt]);
+
+
+    }   // Main Method
+
+}   // Main Class
