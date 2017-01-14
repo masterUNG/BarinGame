6c0d95673889e59d964e27ed9ef408c5bbacae5c
masterUNG <phrombutr@gmail.com>
2017-01-14 23:58:36 +0700

    Initial commit

---
+++ app/src/main/java/appewtc/masterung/baringame/MainActivity.java
@@ -0,0 +1,20 @@
+package appewtc.masterung.baringame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickGoMode(View view) {
+        startActivity(new Intent(MainActivity.this, Mode.class));
+    }
+
+}
---
+++ app/src/main/java/appewtc/masterung/baringame/Mode.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.baringame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode);
+    }
+}
